d9fb31ec28882e3d34398e0ccc20857fde34946b
DenisV <volchekdenisminsk@gmail.com>
2022-12-13 18:09:24 +0300

    Fix lint issues

--- index.js
+++
@@ -1,1 +0,0 @@
-export default (one, two) => 'HELLO FROM PACKAGE';
--- src/index.js
+++ src/index.js
@@ -3,23 +3,23 @@ import { readFileSync } from 'fs';
 const getFileAsString = (path) => {
   const readedFile = readFileSync(path);
   return JSON.parse(readedFile);
-}
+};
 
-export const gendiff = (path1, path2) => {
+export default (path1, path2) => {
   const obj1 = getFileAsString(path1);
   const obj2 = getFileAsString(path2);
 
   const result = ['{'];
 
-  const merge = Object.assign({}, obj1, obj2);
+  const merge = { ...obj1, ...obj2 };
 
-  Object.keys(merge).sort().map(key => {
-    if (!obj2.hasOwnProperty(key)) {
+  Object.keys(merge).sort().forEach((key) => {
+    if (!Object.prototype.hasOwnProperty.call(obj2, key)) {
       result.push(`- ${key}: ${obj1[key]}`);
       return;
     }
 
-    if (!obj1.hasOwnProperty(key)) {
+    if (!Object.prototype.hasOwnProperty.call(obj1, key)) {
       result.push(`+ ${key}: ${obj2[key]}`);
       return;
     }
@@ -32,9 +32,9 @@ export const gendiff = (path1, path2) => {
     }
 
     result.push(`  ${key}: ${obj1[key]}`);
-  })
+  });
 
   result.push('}');
 
   return result.join('\n');
-}
+};
