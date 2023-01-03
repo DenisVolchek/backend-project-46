384225f0e0fd0b0100986d62a97240eb5a98fe25
DenisV <volchekdenisminsk@gmail.com>
2023-01-03 14:12:02 +0300

    Add tests.

--- __tests__/first.test.js
+++ __tests__/first.test.js
@@ -1,4 +1,23 @@
+import path from 'path';
+// import fs from 'fs';
+import { fileURLToPath } from 'url';
+import gendiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__filename);
+console.log(__dirname);
+
+const getFile = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+// const readFile = (filename) => fs.readFileSync(getFile(filename), 'utf-8');
+
 test('reverse', () => {
-  expect('one').toEqual('one');
-  expect('second').toEqual(2);
+  const filePath1 = getFile('file1.json');
+  const filePath2 = getFile('file2.json');
+
+  const result = gendiff(filePath1, filePath2);
+  console.log(result);
+
+  expect(true).toEqual(true);
 });
